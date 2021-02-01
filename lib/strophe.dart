2aa090427389c1423ca5b7669bc5f53205e864ca
Daniil Shumko <daniil.shumko@idt.net>
2021-02-01 15:22:12 +0000

    Exporting StropheHandler

--- lib/strophe.dart
+++ lib/strophe.dart
@@ -3,6 +3,7 @@ library strophe;
 export 'src/bosh/bosh.dart';
 export 'src/core/core.dart';
 export 'src/core/Strophe.Builder.dart';
+export 'src/core/Strophe.Handler.dart';
 export 'src/core/Strophe.Connection.dart';
 export 'src/websocket.dart';
 export 'src/plugins/administration.dart';
